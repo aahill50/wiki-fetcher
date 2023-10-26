359b0fa22f2382c3e29bc661621efd6587728f62
Aaron Hill <aahill50@gmail.com>
2023-10-26 15:31:19 -0400

    set up eslint

---
+++ .eslintrc.js
@@ -0,0 +1,30 @@
+module.exports = {
+    extends: [
+        'eslint:recommended',
+        'plugin:@typescript-eslint/recommended',
+        'plugin:@typescript-eslint/recommended-requiring-type-checking',
+        'plugin:react/recommended',
+        'next/core-web-vitals',
+        'plugin:jest/recommended',
+        'plugin:jest/style',
+    ],
+    overrides: [
+        {
+            env: {
+                node: true,
+            },
+            files: ['.eslintrc.{js,cjs}'],
+            parserOptions: {
+                sourceType: 'script',
+            },
+        },
+    ],
+    parser: '@typescript-eslint/parser',
+    parserOptions: {
+        ecmaVersion: 'latest',
+        project: './tsconfig.json',
+        sourceType: 'module',
+    },
+    plugins: ['jest'],
+    rules: {},
+};
