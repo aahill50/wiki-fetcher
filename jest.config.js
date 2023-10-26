c551b7438285b728b40d6a927ab99910440b72a5
Aaron Hill <aahill50@gmail.com>
2023-10-26 15:16:12 -0400

    set up testing

---
+++ jest.config.js
@@ -0,0 +1,24 @@
+/**
+ * For a detailed explanation regarding each configuration property, visit:
+ */
+
+/** @type {import('jest').Config} */
+const config = {
+    collectCoverage: true,
+    collectCoverageFrom: ['src/**'],
+    coverageDirectory: 'coverage',
+    coverageProvider: 'v8',
+    moduleNameMapper: {
+        '^~/(.*)$': '<rootDir>/src/$1',
+    },
+    setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
+    testEnvironment: 'jsdom',
+    transform: {
+        '^.+\\.tsx?$': [
+            'ts-jest',
+            { tsconfig: '<rootDir>/tsconfig.jest.json' },
+        ],
+    },
+};
+
+module.exports = config;
